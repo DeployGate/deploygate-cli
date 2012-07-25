115384b87a48bbbd7535dbe9c1344d75d5d09b9c
Kyosuke INOUE <kyoro@hakamastyle.net>
2012-07-25 19:44:29 +0900

    dgate

--- dgate.rb
+++ dgate.rb
@@ -8,7 +8,8 @@ require 'pp'
 API_BASE_URL = "http://localhost:3000"
 SETTING_FILE = ENV["HOME"] + "/.dgate"
 $settings = {
-  :token => "aa"
+  'name' => "",
+  'token' => ""
 }
 
 
@@ -16,9 +17,9 @@ def post_request(path,params)
   url = API_BASE_URL + path
   client = HTTPClient.new
   extheaders = []
-  api_token = $settings[:token]
+  api_token = $settings['token']
   unless api_token.nil?
-    extheaders.push(['HTTP_AUTHORIZATION',api_token])
+    extheaders.push(['AUTHORIZATION',api_token])
   end
   res = client.post(url,params,extheaders)
   return nil unless res.status_code == 200
@@ -31,10 +32,11 @@ def get_request(path,params)
   url = API_BASE_URL + path
   client = HTTPClient.new
   extheaders = []
-  api_token = $settings[:token]
+  api_token = $settings['token']
   unless api_token.nil?
-    extheaders.push(['HTTP_AUTHORIZATION',api_token])
+    extheaders.push(['AUTHORIZATION',api_token])
   end
+#params = {'token' => api_token}
   res = client.get(url,params,extheaders)
   return nil unless res.status_code == 200
   res_object = JSON.parse(res.body)
@@ -54,7 +56,6 @@ def do_load_settings
   file = open(SETTING_FILE)
   data = file.read
   file.close
-  pp data
   $settings = JSON.parse(data)
 end
 
@@ -75,7 +76,8 @@ def do_create_session
     print "Invalid email or password.\n"
     exit
   end
-  $settings[:token] = login_res['api_token']
+  $settings['token'] = login_res['api_token']
+  $settings['name'] = login_res['name']
   do_save_settings
   return true
 end
@@ -86,13 +88,65 @@ def do_check_session
     print "Your session was expired or invalid.\n"
     exit unless  do_create_session
   end
+  $settings['name'] = check_res['name']
   return true
 end
 
+def do_push_file
+  file_path = nil
+  target_user = nil
+  if ARGV[2].nil?
+    target_user = $settings['name']
+    file_path = ARGV[1]
+  else
+    target_user = ARGV[1]
+    file_path = ARGV[2]
+  end
+  if file_path.nil? || !File.exist?(file_path)
+    print "target file is not found.\n"
+    exit
+  end
+  push_res = nil
+  open(file_path) do |file|
+    push_res = post_request(
+        sprintf("/api/users/%s/apps",target_user),
+        { :file => file }
+        )
+  end
+  if push_res.nil?
+    print "Sorry, push operation was faild.\n"
+    exit
+  end
+  web_url =  sprintf("%s/users/%s/apps/%s",
+      API_BASE_URL,target_user,push_res['package_name'])
+  #if first app, start to share.
+  if push_res['revision'] == 1
+    share_res = post_request(
+        sprintf(
+          "/api/users/%s/apps/%s/share",
+          target_user,push_res['package_name']),{}
+        )
+    if !share_res['secret'].nil?
+      web_url += sprintf("?key=%s",share_res['secret'])
+    end
+  else
+    if !push_res['secret'].nil?
+      web_url += sprintf("?key=%s",push_res['secret'])
+    end
+  end
+  print "Push app file successful!\n"
+  print "\n"
+  print "Name :\t\t" + push_res['name'] + "\n"
+  print "Owner :\t\t" + push_res['user']['name'] + "\n"
+  print "Package :\t" + push_res['package_name'] + "\n"
+  print "Revision :\t" + push_res['revision'].to_s + "\n"
+  print "URL :\t\t" + web_url
+  print "\n\n"
+  system "open " + web_url
+end
 
-## main
+#### main
 do_load_settings
-
 command = ARGV[0]
 
 if command.nil?
@@ -100,22 +154,25 @@ if command.nil?
   exit
 end
 
+# logout
+if command == 'logout'
+  $settings['token'] = ''
+  do_save_settings
+  print "Session is deleted.\n"
+  exit
+end
 
-if $settings[:token].nil? || $settings[:token] == ""
+# login
+if $settings['token'].nil? || $settings['token'] == ""
   do_create_session
 else
   do_check_session
 end
 
-if ARGV[0] == "push"
-  
+if command == 'push'
+  if ARGV[1].nil?
+    print "Please set target app file.\n"
+    exit
+  end
+  do_push_file
 end
-
-
-
-
-
-
-
-
-
