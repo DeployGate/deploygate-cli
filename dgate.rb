6185f4be947e254038e5fc3870c83ca0a344ef8b
Kyosuke INOUE <kyoro@hakamastyle.net>
2012-07-25 16:26:19 +0900

    dgalte cls

--- dgate.rb
+++ dgate.rb
@@ -1,44 +1,71 @@
 #!/usr/bin/ruby 
 require 'net/http'
 require 'json'
+require 'httpclient'
+require "readline"
 require 'pp'
 
 API_BASE_URL = "http://localhost:3000"
+SETTING_FILE = ENV["HOME"] + "/.dgate"
 $settings = {
-  :token => ""
+  :token => "aa"
 }
 
+
 def post_request(path,params)
-  uri = URI(API_BASE_URL + path)
-  req = Net::HTTP::Post.new(uri.path)
+  url = API_BASE_URL + path
+  client = HTTPClient.new
+  extheaders = []
   api_token = $settings[:token]
   unless api_token.nil?
-    req['HTTP_AUTHORIZATION'] = api_token
-  end 
-  unless params.nil?
-    req.set_form_data(params)
+    extheaders.push(['HTTP_AUTHORIZATION',api_token])
   end
-  res = Net::HTTP.start(uri.host, uri.port) do |http|
-    http.request(req)
-  end
-  return nil unless res.code == "200"
+  res = client.post(url,params,extheaders)
+  return nil unless res.status_code == 200
   res_object = JSON.parse(res.body)
-  pp res_object['error'].to_s
   return nil if res_object['error'] == true
   return res_object['results']
 end
-if ARGV[0].nil?
-  print "please read command guide.\n"
-  exit
+
+def get_request(path,params)
+  url = API_BASE_URL + path
+  client = HTTPClient.new
+  extheaders = []
+  api_token = $settings[:token]
+  unless api_token.nil?
+    extheaders.push(['HTTP_AUTHORIZATION',api_token])
+  end
+  res = client.get(url,params,extheaders)
+  return nil unless res.status_code == 200
+  res_object = JSON.parse(res.body)
+  return nil if res_object['error'] == true
+  return res_object['results']
 end
 
+def do_save_settings
+  data = JSON.generate($settings)
+  file = open(SETTING_FILE,"w+")
+  file.print data
+  file.close
+end
 
-if $settings[:token].nil? || $settings[:token] == ""
-  #login
+def do_load_settings
+  return nil unless File.exist?(SETTING_FILE)
+  file = open(SETTING_FILE)
+  data = file.read
+  file.close
+  pp data
+  $settings = JSON.parse(data)
+end
+
+def do_create_session
   print "Email:"
   email = $stdin.gets.chop
+  system "stty -echo"
   print "Password:"
   password = $stdin.gets.chop
+  print "\n"
+  system "stty echo"
   login_res = post_request(
     '/api/sessions',{
       'email' => email, 
@@ -48,7 +75,36 @@ if $settings[:token].nil? || $settings[:token] == ""
     print "Invalid email or password.\n"
     exit
   end
-  $settings[:token] = login_res['api_token'] 
+  $settings[:token] = login_res['api_token']
+  do_save_settings
+  return true
+end
+
+def do_check_session
+  check_res = get_request('/api/sessions/user',{})
+  if check_res.nil?
+    print "Your session was expired or invalid.\n"
+    exit unless  do_create_session
+  end
+  return true
+end
+
+
+## main
+do_load_settings
+
+command = ARGV[0]
+
+if command.nil?
+  print "Please set dgate command.\n"
+  exit
+end
+
+
+if $settings[:token].nil? || $settings[:token] == ""
+  do_create_session
+else
+  do_check_session
 end
 
 if ARGV[0] == "push"
@@ -58,12 +114,6 @@ end
 
 
 
-
-login_res = post_request(
-    '/api/sessions',{
-      'email' => 'kyoro@hakamastyle.net', 
-      'password' => 'hoge'
-    })
 
 
 
