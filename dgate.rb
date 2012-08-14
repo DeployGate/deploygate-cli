c86634a28f14545325c695f1c8dfdd3c7227d686
Kyosuke INOUE <kyoro@hakamastyle.net>
2012-08-14 17:21:03 +0900

    sig

--- dgate.rb
+++ dgate.rb
@@ -5,8 +5,8 @@ require 'json'
 require 'httpclient'
 require 'pp'
 
-#API_BASE_URL = "https://stg.deploygate.com"
-API_BASE_URL = "http://localhost:3000"
+API_BASE_URL = "https://stg.deploygate.com"
+#API_BASE_URL = "http://localhost:3000"
 #API_BASE_URL = "http://picora.us:8080"
 SETTING_FILE = ENV["HOME"] + "/.dgate"
 $settings = {
