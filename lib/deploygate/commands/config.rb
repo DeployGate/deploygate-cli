22eab46b8c3e2adb38790705cfb8be024631ddcd
henteko <henteko07@gmail.com>
2015-11-22 16:37:46 +0900

    support json format

--- lib/deploygate/commands/config.rb
+++ lib/deploygate/commands/config.rb
@@ -40,39 +40,77 @@ module DeployGate
         # @param [Boolean] json_format
         # @return [void]
         def print_login_success(login_user, json_format)
-          DeployGate::Message::Success.print('Login success')
-          puts ''
-          print_login_user(login_user, json_format)
+          message = 'Login success'
+          data = {:message => message, :error => false}
+
+          unless json_format
+            DeployGate::Message::Success.print(message)
+            puts ''
+          end
+
+          print_login_user(login_user, json_format, data)
         end
 
         # @param [Boolean] json_format
+        # @param [Hash] data
         # @return [void]
-        def print_login_failed(json_format)
-          DeployGate::Message::Error.print('Login failed')
-          puts <<EOF
+        def print_login_failed(json_format, data = {})
+          message = 'Login failed'
+          data[:error]   = true
+          data[:message] = message
+
+          if json_format
+            print_json(data)
+          else
+            DeployGate::Message::Error.print(message)
+            puts <<EOF
 
 Please check your name and api token.
 EOF
+          end
         end
 
         # @param [Boolean] json_format
+        # @param [Hash] data
         # @return [void]
-        def print_not_login(json_format)
-          DeployGate::Message::Warning.print('No user login')
-          puts <<EOF
+        def print_not_login(json_format, data = {})
+          message = 'Not user login'
+          data[:error]   = true
+          data[:message] = message
+
+          if json_format
+            print_json(data)
+          else
+            DeployGate::Message::Warning.print(message)
+            puts <<EOF
 
 Please login to dg command.
 $ dg login
 EOF
+          end
         end
 
         # @param [Hash] login_user
         # @param [Boolean] json_format
+        # @param [Hash] data
         # @return [void]
-        def print_login_user(login_user, json_format)
-          puts <<EOF
-User name: #{login_user['name']}
+        def print_login_user(login_user, json_format, data = {})
+          data[:error] = data[:error].nil? ? false : data[:error]
+          data[:name]  = login_user['name']
+
+          if json_format
+            print_json(data)
+          else
+            puts <<EOF
+User name: #{data[:name]}
 EOF
+          end
+        end
+
+        # @param [Hash] data
+        # @return [void]
+        def print_json(data)
+          puts data.to_json
         end
       end
     end
