8de80b4a62c988a305168f2f42cd764810991b6d
henteko <henteko07@gmail.com>
2015-12-05 15:38:44 +0900

    add distribution key option

--- lib/deploygate/deploy.rb
+++ lib/deploygate/deploy.rb
@@ -12,10 +12,11 @@ module DeployGate
       # @param [String] file_path
       # @param [String] target_user
       # @param [String] message
+      # @param [String] distribution_key
       # @param [Boolean] disable_notify
       # @yield Upload process block
       # @return [Hash]
-      def push(file_path, target_user, message, disable_notify = false, &process_block)
+      def push(file_path, target_user, message, distribution_key, disable_notify = false, &process_block)
         raise NotFileExistError, 'Target file is not found' if file_path.nil? || !File.exist?(file_path)
 
         session = DeployGate::Session.new()
@@ -23,7 +24,7 @@ module DeployGate
         token = session.token
 
 
-        data = API::V1::Push.upload(file_path, target_user, token, message, disable_notify) { process_block.call unless process_block.nil? }
+        data = API::V1::Push.upload(file_path, target_user, token, message, distribution_key || '', disable_notify) { process_block.call unless process_block.nil? }
         raise UploadError, data[:message] if data[:error]
 
         data
