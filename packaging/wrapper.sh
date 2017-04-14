dc184917f3245332b99d8dc4d894686b7a2062a9
henteko <henteko07@gmail.com>
2017-04-14 18:44:59 +0900

    support symlink

--- packaging/wrapper.sh
+++ packaging/wrapper.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 set -e
 
+cd `dirname $0`
+TARGET_FILE=`basename $0`
+
+while [ -L "$TARGET_FILE" ]
+do
+  TARGET_FILE=`readlink $TARGET_FILE`
+  cd `dirname $TARGET_FILE`
+  TARGET_FILE=`basename $TARGET_FILE`
+done
+
+PHYS_DIR=`pwd -P`
+RESULT=$PHYS_DIR/$TARGET_FILE
+
 # Figure out where this script is located.
-SELFDIR="`dirname \"$0\"`"
+SELFDIR="`dirname \"$RESULT\"`"
 SELFDIR="`cd \"$SELFDIR\" && pwd`"
 
 # Tell Bundler where the Gemfile and gems are.
