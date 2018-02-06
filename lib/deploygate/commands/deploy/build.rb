e15ebe70c0eb6a6e55623cff7744a616f7b2981c
henteko <henteko07@gmail.com>
2018-02-06 16:10:41 +0900

    add allowProvisioningUpdates

--- lib/deploygate/commands/deploy/build.rb
+++ lib/deploygate/commands/deploy/build.rb
@@ -48,13 +48,18 @@ module DeployGate
 
             codesigning_identity= nil
             provisioning_style = analyze.provisioning_style
-            if (!over_xcode_8? && provisioning_style == nil) ||
+            if (!over_xcode?(8) && provisioning_style == nil) ||
                 provisioning_style == DeployGate::Xcode::Analyze::PROVISIONING_STYLE_MANUAL
               # Only run Provisioning Style is Manual or nil
               codesigning_identity = DeployGate::Xcode::Export.codesigning_identity(target_provisioning_profile)
             end
 
-            ipa_path = DeployGate::Xcode::Ios.build(analyze, target_scheme, codesigning_identity, build_configuration, method)
+            ipa_path = DeployGate::Xcode::Ios.build(analyze,
+                                                    target_scheme,
+                                                    codesigning_identity,
+                                                    build_configuration,
+                                                    method,
+                                                    over_xcode?(9))
             Push.upload([ipa_path], options)
           end
 
@@ -64,14 +69,14 @@ module DeployGate
             puts ''
           end
 
-          def over_xcode_8?
+          def over_xcode?(version_number)
             version = Gym::Xcode.xcode_version
             if version == nil
               print_no_install_xcode
               exit 1
             end
 
-            version.split('.')[0].to_i >= 8
+            version.split('.')[0].to_i >= version_number
           end
 
           def print_no_install_xcode
