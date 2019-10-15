7f64d83f548b59d31cda77a577f63d19ff389f80
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-10-15 12:08:48 +0900

    Fixed bundle command check and removed some branch

--- install.bash
+++ install.bash
@@ -5,6 +5,7 @@ set -o pipefail
 
 export MIN_RUBY_VERSION="2.4.0"
 export DG_DIRECTORY="$HOME/.dg"
+export DG_BIN_DIRECTORY_NAME="link"
 
 readonly BOLD="$(tput bold)"
 readonly GREEN="$(tput setaf 2)"
@@ -40,7 +41,7 @@ pending() {
   echo -e "- [-] $*"
 }
 
-mkdir -p "${DG_DIRECTORY}/link"
+mkdir -p "${DG_DIRECTORY}/${DG_BIN_DIRECTORY_NAME}"
 
 # Change the working directory to know which ruby version being used in the deploygate's directory.
 pushd "${DG_DIRECTORY}" >/dev/null
@@ -51,13 +52,11 @@ checking "Checking ruby..."
 
 if ! type "ruby" >/dev/null 2>&1; then
   abort 'ruby is not found'
-  warn "ruby is required to install this command. Please make sure ruby is installed."
+  warn "ruby is required to install dg. Please make sure ruby is installed."
   exit 1
 fi
 
 ok "ruby is installed."
-
-readonly ruby_version="$(ruby -e 'puts RUBY_VERSION')"
 
 checking "Checking ruby's version..."
 
@@ -73,17 +72,17 @@ ok "ruby's version ($(ruby --version)) is enough."
 
 checking "Checking bundler..."
 
-if ! type "bundler" >/dev/null 2>&1; then
-  abort "bundler is not found"
-  warn "bundler is required to install this command. Please run ${BOLD}gem install bundler${RESET} to install bundle command"
+if ! type "bundle" >/dev/null 2>&1; then
+  abort "bundle is not found"
+  warn "bundle command is required to install dg. Please run ${BOLD}gem install bundler${RESET} to install it."
   exit 1
 fi
 
-ok "bundler ($(bundler --version)) is installed"
+ok "bundler ($(bundler --version)) is installed."
 
 cat<<EOF
 
-This script will install dg to ${DG_DIRECTORY}/link ...
+This script will install dg to ${DG_DIRECTORY}/${DG_BIN_DIRECTORY_NAME} ...
 
 EOF
 
@@ -95,7 +94,7 @@ if ([ -L '/usr/local/bin/dg' ] || [ -f '/usr/local/bin/dg' ]) && [ ! -f 'Gemfile
   exit 1
 fi
 
-if [ -f 'link/dg' ]; then
+if [ -f "${DG_BIN_DIRECTORY_NAME}/dg" ]; then
   pending 'it sounds dg has already been installed.'
   read -p "Would you like to upgrade dg to the newer version or install dg under the new ruby version? (y/N): " yn
 
@@ -120,30 +119,30 @@ EOF
 
 sleep 1s
 
+readonly ruby_version="$(ruby -e 'puts RUBY_VERSION')"
 readonly ruby_abi=$(echo $ruby_version | cut -d. -f1-2).0
 
-if [ -f 'link/dg' ] && [ -d "vendor/bundle/ruby/$ruby_abi" ]; then
-  bundle upgrade deploygate
-else
-  if [ ! -f "Gemfile" ]; then
-    bundle init
-    echo >> Gemfile; echo "gem 'deploygate'" >> Gemfile
-  fi
+if [ ! -f "Gemfile" ]; then
+  bundle init
+  echo >> Gemfile; echo "gem 'deploygate'" >> Gemfile
+fi
 
-  # NOTE: this option is available since bundler 2.x
-  bundle config --local global_path_appends_ruby_scope true >/dev/null
-  bundle config --local disable_shared_gems true >/dev/null
+# NOTE: this option is available since bundler 2.x
+bundle config --local global_path_appends_ruby_scope true >/dev/null
+bundle config --local disable_shared_gems true >/dev/null
 
-  bundle install --jobs=4 --clean --retry 3 --path=vendor/bundle
+bundle check || bundle install --jobs=4 --clean --retry 3 --path=vendor/bundle
+bundle update deploygate
 
-  cat<<EOS > link/dg
+cat<<EOS > "${DG_BIN_DIRECTORY_NAME}/dg"
 #!/usr/bin/env bash
 
 set -eu
+set -o pipefail
 
-if ! \$(ruby -e "exit 1 unless RUBY_VERSION == '$ruby_version')"); then
+if \$(ruby -e "exit 1 unless RUBY_VERSION == '$ruby_version')"); then
   echo 'dg has been installed to $ruby_version but not to \$(ruby --version)' 1>&2
-  echo 'Please run "cd $DG_DIRECTORY && bundle install" to install dg into the current ruby version' 1>&2
+  echo 'Please run "cd $DG_DIRECTORY && bundle install" manually to re-install dg into the current ruby version' 1>&2
   exit 1
 fi
 
@@ -159,11 +158,11 @@ export BUNDLE_GLOBAL_PATH_APPENDS_RUBY_SCOPE=true
 exec bundle exec dg "\$@"
 EOS
 
-  chmod +x link/dg
-  ln -sf "${DG_DIRECTORY}/link/dg" /usr/local/bin/dg
+chmod +x "${DG_BIN_DIRECTORY_NAME}/dg"
+ln -sf "${DG_DIRECTORY}/${DG_BIN_DIRECTORY_NAME}/dg" /usr/local/bin/dg
 
-  echo -e "${GREEN}Welcome to DeployGate!${RESET}"
-  cat <<'EOF'
+echo -e "${GREEN}Welcome to DeployGate!${RESET}"
+cat <<'EOF'
         _            _                       _
       | |          | |                     | |
     __| | ___  ___ | | ___ _   ,____   ___ | |_ ___
