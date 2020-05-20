2a23bd0a6ad7d4c9d3fb8c6d92966224f7663d64
henteko <henteko07@gmail.com>
2020-05-21 01:52:59 +0900

    fixed tests

--- spec/deploygate/xcode/member_center_spec.rb
+++ spec/deploygate/xcode/member_center_spec.rb
@@ -5,7 +5,7 @@ describe DeployGate::Xcode::MemberCenter do
   end
 
   let(:email) { 'test@example.com' }
-  let(:center) { DeployGate::Xcode::MemberCenter.instance }
+  let(:center) { DeployGate::Xcode::MemberCenter.new('com-example-team-id') }
   before do
     allow_any_instance_of(Spaceship::PortalClient).to receive(:login) {}
     allow_any_instance_of(Spaceship::PortalClient).to receive(:teams) {['team_name', 'team_id']}
