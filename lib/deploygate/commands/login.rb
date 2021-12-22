82db1dfa71ed7868690128c5149f29c53b0279be
jiikko <n905i.1214@gmail.com>
2021-12-22 19:28:36 +0900

    新機登録銅線系のリソースを削除する

--- lib/deploygate/commands/login.rb
+++ lib/deploygate/commands/login.rb
@@ -58,42 +58,6 @@ module DeployGate
         end
 
         # @return [String]
-        def input_new_account_name
-          user_name = ask(I18n.t('commands.login.input_new_account_name.input_user_name'))
-          print I18n.t('commands.login.input_new_account_name.checking')
-
-          if DeployGate::User.registered?(user_name, '')
-            puts HighLine.color(I18n.t('commands.login.input_new_account_name.already_used_user_name', user_name: user_name), HighLine::RED)
-            return input_new_account_name()
-          else
-            puts HighLine.color(I18n.t('commands.login.input_new_account_name.success', user_name: user_name), HighLine::GREEN)
-            return user_name
-          end
-        end
-
-        # @return [String]
-        def input_new_account_password
-          password = input_password(I18n.t('commands.login.input_new_account_password.input_password'))
-          secound_password = input_password(I18n.t('commands.login.input_new_account_password.input_same_password'))
-
-          if password == secound_password
-            return password
-          else
-            puts HighLine.color(I18n.t('commands.login.input_new_account_password.error'), HighLine::RED)
-            return input_new_account_password()
-          end
-        end
-
-        # @return [boolean]
-        def check_terms
-          puts I18n.t('commands.login.check_terms.terms_url')
-          puts I18n.t('commands.login.check_terms.privacy_url')
-          puts I18n.t('commands.login.check_terms.note')
-          puts ''
-          HighLine.agree(I18n.t('commands.login.check_terms.text')) {|q| q.default = "n"}
-        end
-
-        # @return [String]
         def input_password(message)
           ask(message) { |q| q.echo = "*" }
         end
--- lib/deploygate/user.rb
+++ lib/deploygate/user.rb
@@ -10,17 +10,6 @@ module DeployGate
 
     # @param [String] name
     # @param [String] email
-    # @param [String] password
-    # @return [DeployGate::User]
-    def self.create(name, email, password)
-      locale = Locale.current.language
-      results = DeployGate::API::V1::User.create(name, email, password, locale)
-      return if results[:error]
-      DeployGate::User.new(results[:name])
-    end
-
-    # @param [String] name
-    # @param [String] email
     # @return [Boolean]
     def self.registered?(name, email)
       DeployGate::API::V1::User.registered?(name, email)
