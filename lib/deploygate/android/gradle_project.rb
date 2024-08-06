06677e5208c498c2ff097c905ea5475f14da28b6
Jumpei Matsuda <jmatsu.drm@gmail.com>
2024-08-06 19:01:27 +0900

    fix: File#exists? has been removed since Ruby 3.2

--- lib/deploygate/android/gradle_project.rb
+++ lib/deploygate/android/gradle_project.rb
@@ -6,7 +6,7 @@ module DeployGate
         # @param [String] dir
         # @return [Boolean]
         def root_dir?(dir)
-          File.exists? File.join(dir, 'settings.gradle')
+          File.exist?(File.join(dir, 'settings.gradle'))
         end
       end
     end
